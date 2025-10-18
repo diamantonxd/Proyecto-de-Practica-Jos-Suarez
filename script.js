071b223e010528a60d718a8464ff97341156ea5a
diamantonxd <diamantonxd@gmail.com>
2025-10-17 20:10:58 -0400

    modificar index.html y script.js

---
+++ script.js
@@ -0,0 +1,53 @@
+let MostrarValor = ' ';
+let OperacionActual = null;
+let PrimeraOperacion = null;
+
+function BotonNro(Nro) {
+    MostrarValor += Nro;
+    ActualizarDisplay(MostrarValor);
+}
+
+function PonerOperacion(Operacion) {
+    if (OperacionActual !== null) {
+        CalcularOperacion();
+    }
+    PrimeraOperacion = parseFloat(MostrarValor);
+    OperacionActual = Operacion;
+    MostrarValor = ' ';
+}
+
+function CalcularOperacion() {
+    if (OperacionActual === null || MostrarValor === ' ') return;
+    const SegundaOperacion = parseFloat(MostrarValor);
+    switch (OperacionActual) {
+        case '+': MostrarValor = (PrimeraOperacion + SegundaOperacion).toString();
+            break;
+        case '-': MostrarValor = (PrimeraOperacion - SegundaOperacion).toString();
+            break;
+        case '*': MostrarValor = (PrimeraOperacion * SegundaOperacion).toString();
+            break;
+        case '/': MostrarValor = (PrimeraOperacion / SegundaOperacion).toString();
+            break;
+    }
+    ActualizarDisplay(MostrarValor);
+    OperacionActual = null;
+}
+
+function LimpearDispley() {
+    MostrarValor = ' ';
+    OperacionActual = null;
+    PrimeraOperacion = null;
+    ActualizarDisplay(MostrarValor);
+}
+
+function ActualizarDisplay(Valor) {
+    document.getElementById('CalculadoraDisplay').value = Valor;
+}
+
+function EliminarValor() {
+    MostrarValor = MostrarValor.slice(0, -1);
+    ActualizarDisplay(MostrarValor);
+}
+
+
+
